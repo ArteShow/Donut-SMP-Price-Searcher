222b5fa20e10ba8a05f1ce43a2632f6e713bd641
ArteShow <sokartemax@gmail.com>
2026-01-23 16:22:45 +0100

    add calculate package

--- pkg/calculate/price.go
+++ pkg/calculate/price.go
@@ -1,1 +1,10 @@
 package calculate
+
+func CalculateAveragePrice(items []float64) float64 {
+	var sum float64
+	for _, price := range items {
+		sum += price
+	}
+	
+	return sum / float64(len(items))
+}
