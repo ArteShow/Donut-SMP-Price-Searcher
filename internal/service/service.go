d124eb62ce219c9013daad1feb58fe92cc4bc884
ArteShow <sokartemax@gmail.com>
2026-01-23 17:36:08 +0100

    add service package

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,27 @@
 package service
+
+import (
+	"log"
+
+	"github.com/ArteShow/Donut-SMP-Price-Searcher/internal/requester"
+)
+
+func GetLowestAveragePricePerItem(item, token string) (float64, error) {
+	page := 1
+	items := 0
+	var prices []float64
+	for {
+		if items < 20 {
+			resp, err := requester.GetAhObjects(token, "lowest_price", item, page)
+			if err != nil {
+				break
+			}
+
+			log.Println(prices, resp)
+
+		}else {
+			break
+		}
+		
+	}
+}
