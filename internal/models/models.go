0080846257032b0ba71544a8da1168add013102e
ArteShow <sokartemax@gmail.com>
2026-01-23 16:02:10 +0100

    add models package

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,1 +1,20 @@
 package models
+
+type GetAuctionStatsRequest struct {
+	Sort   string `json:"sort"`
+	Search string `json:"searcher"`
+}
+
+type GetAuctionStatsResponse struct {
+	Items []AhObject `json:"result"`
+}
+
+type AhObject struct {
+	Price float64 `json:"price"`
+	Item  AhItem  `json:"item"`
+}
+
+type AhItem struct {
+	ID    string  `json:"id"`
+	Count float64 `json:"count"`
+}
